f1dcfa865e67c4c08a7dddfe6b49c001b1932367
Matte553 <91864594+Matte553@users.noreply.github.com>
2022-09-26 16:23:07 +0200

    updated formpage

--- testvue/src/views/FormPage.vue
+++ testvue/src/views/FormPage.vue
@@ -3,20 +3,101 @@
   <v-app>
     <v-card elevation="5" width="80%" class="mx-auto mt-5">
         <v-tabs
+          v-model='tab'
           background-color="deep-purple accent-4"
           center-active
-          dark
-        >
-        <v-tab> Economical </v-tab>
-        <v-tab> Social </v-tab>
-        <v-tab> Enviromental</v-tab>
-        <v-tab> Result </v-tab>
+          dark >
+
+          <!-- ############################################################# TAB 1 -->
+        <v-tab ripple href='#tab-1'> Enviromental </v-tab>
+          <v-tab-item value='tab-1'> 
+            <v-card-text class="text-left font-weight-black my-5" v-show="currentTab == 1">
+
+              <p> This is a question about commuting, how do you plan to commute to work?: </p>
+              <!-- rows margin-y-axis-5, x-axis-2- -->
+              <v-row class="my-5 mx-2">
+                <v-btn-toggle
+                  v-model="multipleQ1"
+                  multiple>
+                  <v-btn elevation="2" class="mx-2"> No </v-btn>
+                  <v-btn elevation="2" class="mx-2"> Probably not </v-btn>
+                  <v-btn elevation="2" class="mx-2"> Probably </v-btn>
+                  <v-btn elevation="2" class="mx-2"> Yes </v-btn>
+                </v-btn-toggle>
+              </v-row>
+
+              <p> This is Eco question two: </p>
+              <v-row class="my-8 mx-2">
+                <v-btn-toggle>
+                  <v-btn elevation="2" class="mx-2"> No </v-btn>
+                  <v-btn elevation="2" class="mx-2"> Probably not </v-btn>
+                  <v-btn elevation="2" class="mx-2"> Probably </v-btn>
+                  <v-btn elevation="2" class="mx-2"> Yes </v-btn>
+                </v-btn-toggle>
+              </v-row>
+
+              <v-row class="mx-2">
+                <v-btn class="mx-2" depressed color="primary" large @click="prevTab"> Prev </v-btn>
+                <v-btn class="mx-2" depressed color="success" large @click="nextTab"> Next </v-btn>
+              </v-row>
+
+              </v-card-text> 
+          </v-tab-item>
+        <!-- ############################################################# TAB 1 -->
+        
+
+        <v-tab ripple href='#tab-2'> Social </v-tab>
+          <v-tab-item value='tab-2'> 
+            <v-row class="mx-2">
+              <v-btn class="mx-2" depressed color="primary" large @click="prevTab"> Prev </v-btn>
+              <v-btn class="mx-2" depressed color="success" large @click="nextTab"> Next </v-btn>
+            </v-row>
+            <h1> test2 </h1>
+          </v-tab-item>
+
+        <v-tab ripple href='#tab-3'> Economical </v-tab>
+          <v-tab-item value='tab-3'> 
+            <v-row class="mx-2">
+              <v-btn class="mx-2" depressed color="primary" large @click="prevTab"> Prev </v-btn>
+              <v-btn class="mx-2" depressed color="success" large @click="nextTab"> Next </v-btn>
+            </v-row>
+            <h1> test3 </h1>
+          </v-tab-item>
+
+        <v-tab ripple href='#tab-4'> Other </v-tab>
+          <v-tab-item value='tab-4'> 
+            <v-row class="mx-2">
+              <v-btn class="mx-2" depressed color="primary" large @click="prevTab"> Prev </v-btn>
+              <v-btn class="mx-2" depressed color="success" large @click="nextTab"> Next </v-btn>
+            </v-row>
+            <h1> test4 </h1>
+          </v-tab-item>
+
       </v-tabs>
 
-      <v-card-text class="text-left font-weight-black my-5">
+      <!-- Economical Questions -->
+
+
+      <!-- <v-card-text class="text-left font-weight-black my-5" v-show="currentStep == 2">
+        <p> This is Soc question one: </p>
+        <p> This is Soc question two: </p>
+      </v-card-text>
+
+      <v-card-text class="text-left font-weight-black my-5" v-show="currentStep == 3">
+        <p> This is Enc question one: </p>
+        <p> This is Enc question two: </p>
+      </v-card-text>
+
+      <v-card-text class="text-left font-weight-black my-5" v-show="currentStep == 4">
+        <p> This is Other question one: </p>
+        <p> This is Other question two: </p>
+      </v-card-text> -->
+
+      <!-- 
+      <v-card-text class="text-left font-weight-black my-5" v-show="currentStep == 1">
         <p> This is question one: </p>
         
-        <!--rows margin-y-axis-5, x-axis-2--->
+        rows margin-y-axis-5, x-axis-2-
         <v-row class="my-5 mx-2">
           <v-btn elevation="2" class="mx-2"> No </v-btn>
           <v-btn elevation="2" class="mx-2"> Probably not </v-btn>
@@ -42,8 +123,7 @@
           </v-col>
 
         <p> This is question Four: </p>
-        
-      </v-card-text>
+      </v-card-text> -->
 
     </v-card>
   </v-app>
@@ -56,14 +136,42 @@
       name: 'FormPage',
   
       data: () => ({
+        tab: 'tab-1',
+        multipleQ1: [],
+        currentTab: 1,
         value: 0,
         tickLabels: ['Terrible!', 'Bad', 'Okay', 'Good', 'Excellent!'],
       }),
+
+      methods: {
+        prevTab(){
+          if(this.currentTab != 1){
+            this.currentTab -= 1;
+          }
+          switch(this.currentTab){
+            case 1: this.tab = 'tab-1'; break;
+            case 2: this.tab = 'tab-2'; break;
+            case 3: this.tab = 'tab-3'; break;
+            case 4: this.tab = 'tab-4'; break;
+          }
+          console.log(this.currentTab);
+        },
+        nextTab(){
+          if(this.currentTab != 4){
+            this.currentTab += 1;
+          }
+          switch(this.currentTab){
+            case 1: this.tab = 'tab-1'; break;
+            case 2: this.tab = 'tab-2'; break;
+            case 3: this.tab = 'tab-3'; break;
+            case 4: this.tab = 'tab-4'; break;
+          }
+          console.log(this.currentTab);
+        }
+      }
     }
   </script>
   
 <style>
-    .input{
-        
-    }
+
 </style>
