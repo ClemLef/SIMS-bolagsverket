2b214df53607cf11184c528f5433e5749c404689
Matte553 <91864594+Matte553@users.noreply.github.com>
2022-09-26 13:35:00 +0200

    Updated FormPage /w slider, buttons, v-card, tabs

--- testvue/src/views/FormPage.vue
+++ testvue/src/views/FormPage.vue
@@ -1,5 +1,54 @@
 <template>
-    <h1> Hello world </h1>
+
+  <v-app>
+    <v-card elevation="5" width="80%" class="mx-auto mt-5">
+        <v-tabs
+          background-color="deep-purple accent-4"
+          center-active
+          dark
+        >
+        <v-tab> Economical </v-tab>
+        <v-tab> Social </v-tab>
+        <v-tab> Enviromental</v-tab>
+        <v-tab> Result </v-tab>
+      </v-tabs>
+
+      <v-card-text class="text-left font-weight-black my-5">
+        <p> This is question one: </p>
+        
+        <!--rows margin-y-axis-5, x-axis-2--->
+        <v-row class="my-5 mx-2">
+          <v-btn elevation="2" class="mx-2"> No </v-btn>
+          <v-btn elevation="2" class="mx-2"> Probably not </v-btn>
+          <v-btn elevation="2" class="mx-2"> Probably </v-btn>
+          <v-btn elevation="2" class="mx-2"> Yes </v-btn>
+        </v-row>
+        
+        <p> This is question Two: </p>
+
+        <v-slider class="my-5"
+          v-model="value"
+          step="25"
+          ticks="always"
+          :tick-labels="tickLabels"
+        > </v-slider>
+
+        <p> This is question Three: </p>
+
+        <v-col cols="12" sm="3" md="8" >
+            <v-text-field label="Solo" solo>
+
+            </v-text-field>
+          </v-col>
+
+        <p> This is question Four: </p>
+        
+      </v-card-text>
+
+    </v-card>
+  </v-app>
+
+
 </template>
   
   <script>
@@ -7,57 +56,14 @@
       name: 'FormPage',
   
       data: () => ({
-        ecosystem: [
-          {
-            text: 'vuetify-loader',
-            href: 'https://github.com/vuetifyjs/vuetify-loader',
-          },
-          {
-            text: 'github',
-            href: 'https://github.com/vuetifyjs/vuetify',
-          },
-          {
-            text: 'awesome-vuetify',
-            href: 'https://github.com/vuetifyjs/awesome-vuetify',
-          },
-        ],
-        importantLinks: [
-          {
-            text: 'Documentation',
-            href: 'https://vuetifyjs.com',
-          },
-          {
-            text: 'Chat',
-            href: 'https://community.vuetifyjs.com',
-          },
-          {
-            text: 'Made with Vuetify',
-            href: 'https://madewithvuejs.com/vuetify',
-          },
-          {
-            text: 'Twitter',
-            href: 'https://twitter.com/vuetifyjs',
-          },
-          {
-            text: 'Articles',
-            href: 'https://medium.com/vuetify',
-          },
-        ],
-        whatsNext: [
-          {
-            text: 'Explore components',
-            href: 'https://vuetifyjs.com/components/api-explorer',
-          },
-          {
-            text: 'Select a layout',
-            href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-          },
-          {
-            text: 'Frequently Asked Questions',
-            href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-          },
-        ],
+        value: 0,
+        tickLabels: ['Terrible!', 'Bad', 'Okay', 'Good', 'Excellent!'],
       }),
     }
   </script>
   
+<style>
+    .input{
+        
+    }
+</style>
