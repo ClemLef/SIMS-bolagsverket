c3ccdbcc59a0bed1179c46f1a4d71f634884390f
ClemLef <clem.lef76@gmail.com>
2022-10-09 16:23:52 +0000

    Add questions get api

--- API/routes/api.php
+++ API/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Questions;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('questions', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Questions::all();
+});
+ 
+Route::get('questions/{id}', function($id) {
+    return Questions::find($id);
 });
