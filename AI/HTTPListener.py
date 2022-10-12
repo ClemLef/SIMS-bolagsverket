2b114be813f5d4122fdaa38d26e8e466eb22f148
ClemLef <clement.lef@neuf.fr>
2022-10-12 13:48:52 +0000

    modif listener

--- AI/HTTPListener.py
+++ AI/HTTPListener.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors=CORS(app)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def result():
     print(request.json)  # json (if content-type of application/json is sent with the request)
+    return "<p>Hello, World!</p>"
+
+
 
 if __name__ == "__main__":
     app.run()
