8c4e0d3f349d364f550a627f4ec30e6143b12666
Clément <clement.lef@neuf.fr>
2022-10-12 15:59:19 +0200

    Adding /post page

--- AI/HTTPListener.py
+++ AI/HTTPListener.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 cors=CORS(app)
 
 @app.route('/')
-def result():
-    print(request.json)  # json (if content-type of application/json is sent with the request)
+def index():
     return "<p>Hello, World!</p>"
 
+@app.route('/post')
+def result():
+    print(request.json)  # json (if content-type of application/json is sent with the request)
 
 
 if __name__ == "__main__":
