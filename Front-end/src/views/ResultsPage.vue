3e83d51ebe027adfb350b6ad8e001df6be6e5d09
Clément <clement.lef@neuf.fr>
2022-10-21 16:46:37 +0200

    adding code text field and sharing button

--- Front-end/src/views/ResultsPage.vue
+++ Front-end/src/views/ResultsPage.vue
@@ -12,18 +12,19 @@
 
             <v-tooltip top color="green darken-4">
                 <template v-slot:activator="{ on, attrs }">
-                    <v-btn icon class="my-auto mr-5" :disabled='isDisabled' v-bind="attrs" v-on="on">
+                    <v-btn icon class="my-auto mr-5" :disabled='isDisabled' @click="shareResult()" v-bind="attrs"
+                        v-on="on">
                         <v-icon>mdi-share-variant</v-icon>
                     </v-btn>
                 </template>
-                <span >Share this result</span>
+                <span>Share this result</span>
             </v-tooltip>
 
 
 
         </v-tabs>
-        
-    <component :is="formTaken"></component>
+
+        <component :is="formTaken"></component>
     </v-card>
 </template>
 
@@ -33,53 +34,70 @@
 <script>
 
 import ResultsNotVisible from '@/components/ResultsNotVisible.vue';
-import ResultsVisible from '../components/ResultsVisible.vue'  
+import ResultsVisible from '../components/ResultsVisible.vue'
 
-  
+
 export default {
 
-    components :{
+    components: {
         // eslint-disable-next-line
-      ResultsVisible,
-      // eslint-disable-next-line
-      ResultsNotVisible, 
+        ResultsVisible,
+        // eslint-disable-next-line
+        ResultsNotVisible,
     },
 
     data: () => ({
         buttonDisabled: true,
         cards: [
-            { title: 'What is sustainable investing ?', img_src: 'https://cloudinary.hbs.edu/hbsit/image/upload/s--YvTQW8XL--/f_auto,c_fill,h_375,w_750,/v20200101/9DD1E295483AB53C37A95A0D4BFE6F5D.jpg', source: "online.hbs.edu", description: "Investors can use several strategies to build and diversify their portfolios to ensure financial success. One emerging trend changing the way businesses and investors think about investing is a concept known as sustainable investing.", link: "https://online.hbs.edu/blog/post/sustainable-investing" },
+            /* { title: 'What is sustainable investing ?', img_src: 'https://cloudinary.hbs.edu/hbsit/image/upload/s--YvTQW8XL--/f_auto,c_fill,h_375,w_750,/v20200101/9DD1E295483AB53C37A95A0D4BFE6F5D.jpg', source: "online.hbs.edu", description: "Investors can use several strategies to build and diversify their portfolios to ensure financial success. One emerging trend changing the way businesses and investors think about investing is a concept known as sustainable investing.", link: "https://online.hbs.edu/blog/post/sustainable-investing" },
             { title: 'F-skatt', img_src: 'https://skatteverket.se/images/18.1c68351d170ce554527e35/1584713519347/Logo_vit_bakgrund_FB.png', source: "skatteverket.se", description: "Om du bedriver näringsverksamhet i Sverige kan du efter ansökan hos Skatteverket bli godkänd för F-skatt", link: "https://www.skatteverket.se/foretag/etjansterochblanketter/svarpavanligafragor/fskatt/foretagfskattfaq/vemkanbligodkandforfskattochvadinnebardet.5.18e1b10334ebe8bc8000118949.html" },
-            { title: 'Sustainable funds', img_src: 'https://www.fondbolagen.se/globalassets/om-oss/hallbarhet-artikelbild2-ny-webb.jpg?preset=jumbotron', source: "fondbolagen.se", description: "Knowing that the money they invest contributes to sustainable development is becoming increasingly important to savers.", link: "https://www.fondbolagen.se/en/Facts_Indices/sustainable-funds/" },
+            { title: 'Sustainable funds', img_src: 'https://www.fondbolagen.se/globalassets/om-oss/hallbarhet-artikelbild2-ny-webb.jpg?preset=jumbotron', source: "fondbolagen.se", description: "Knowing that the money they invest contributes to sustainable development is becoming increasingly important to savers.", link: "https://www.fondbolagen.se/en/Facts_Indices/sustainable-funds/" }, */
         ],
-        
+
     }),
     methods: {
         redirectLink(link) {
             window.open(link);
         },
-        
+        createUrl() {
+            let baseUrl = window.location.href
+            let params = '?' + window.$cookies.get('isSustainable').code
+            console.log(baseUrl + params)
+            return baseUrl + params;
+        },
+        shareResult() {
+            let shareUrl = this.createUrl()
+            navigator.clipboard.writeText(shareUrl);
+        }
+
+
     },
     computed: {
-        formTaken(){
+        formTaken() {
             // if a result cookie is present
             //console.log(document.cookie);
-            if(window.$cookies.get('isSustainable') != null){
+            if (window.$cookies.get('isSustainable') != null) {
                 return "ResultsVisible";
             } else {
                 return "ResultsNotVisible";
             }
-        }, 
-        isDisabled(){
+        },
+        isDisabled() {
             // if a result cookie is present
-            if(window.$cookies.get('isSustainable') != null){
+            if (window.$cookies.get('isSustainable') != null) {
                 // enable the buttons and return the results view
                 return false;
             } else {
                 // disable the buttons and return the error page
                 return true;
             }
-        }
+        },
+        computed: {
+            getCookie() {
+                return window.$cookies.get('isSustainable');
+            },
+        },
+
     },
 }
 </script>
