b2ad133ee16cab8bc65bf6d89ef449b31848ff5d
Clément <clement.lef@neuf.fr>
2022-10-21 12:21:06 +0200

    added fillable

--- API/app/Models/AiResults.php
+++ API/app/Models/AiResults.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class AiResults extends Model
 {
     use HasFactory;
+    protected $fillable = ['result_code', 'global', 'social_flag', 'economical_flag', 'environment_flag', 'influence_flag', 'active'];
 }
