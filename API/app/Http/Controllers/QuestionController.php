025f436827451392b6fcfc77aab3d18e13e075f3
Clemlef <clement.lefebvre2@groupe-esigelec.org>
2022-11-02 14:21:40 +0100

    test adding controllers to questions API

--- API/app/Http/Controllers/QuestionController.php
+++ API/app/Http/Controllers/QuestionController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Questions;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function index()
+    {
+        return Questions::all();
+    }
+
+    public function show($id)
+    {
+        return Questions::find($id);
+    }
 }
--- API/routes/api.php
+++ API/routes/api.php
@@ -23,7 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('questions', function() {
+Route::get('questions', 'QuestionController@index');
+Route::get('questions/{id}', 'QuestionController@show');
+
+/* Route::get('questions', function() {
     // If the Content-Type and Accept headers are set to 'application/json', 
     // this will return a JSON structure. This will be cleaned up later.
     return Questions::all();
@@ -31,7 +34,7 @@ Route::get('questions', function() {
  
 Route::get('questions/{id}', function($id) {
     return Questions::find($id);
-});
+}); */
 
 Route::get('answer_sets', function() {
     // If the Content-Type and Accept headers are set to 'application/json', 
